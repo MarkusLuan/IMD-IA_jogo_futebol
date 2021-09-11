d611db5c7921d242575583771dd822a98d0cd114
Markus Luan <markus23_luan@hotmail.com>
2021-09-11 12:18:56 -0300

    Implementando algoritmo de chegada

--- Assets/Scripts/SockerPlayerBehavior.cs
+++ Assets/Scripts/SockerPlayerBehavior.cs
@@ -8,6 +8,7 @@ public class SockerPlayerBehavior : MonoBehaviour
     public float velocidadeMaxima;
     public float maxSteeringForce; // Força maxima de navegacao / Quanto maior mais rapido vira para o destino
     public float stopVal; // Limite de velocidade para considerar como parado
+    public float distChegada;
 
     private Rigidbody2D rigidbody;
 
@@ -26,18 +27,44 @@ public class SockerPlayerBehavior : MonoBehaviour
         else rigidbody.rotation = 360 - angulo;
     }
 
+    Vector2 SetMagnetude (Vector2 v, float max)
+    {
+        return v.normalized * max;
+    }
+
+    // Algoritmo de busca
     Vector2 Seek()
     {
-        Vector2 v = (goal - rigidbody.position).normalized * velocidadeMaxima;
+        Vector2 v = SetMagnetude((goal - rigidbody.position), velocidadeMaxima);
         Vector2 steering = v - rigidbody.velocity;
 
         // Limita o valor máximo da força do steering
         return Vector2.ClampMagnitude(steering, maxSteeringForce);
     }
 
+    // Algoritmo de chegada ao destino
+    Vector2 Arrival()
+    {
+        float dist = Vector2.Distance(rigidbody.position, goal);
+
+        if (dist < stopVal) // Encontra-se muito proximo ao alvo
+        { // Força contraria ao jogador para fazê-lo parar
+            return -rigidbody.velocity;
+        }
+        else if (dist < distChegada) // Encontra-se na area de freio (onde deve começar a parar)
+        {
+            Vector2 v = SetMagnetude((goal - rigidbody.position), velocidadeMaxima);
+            v *= dist / distChegada;
+            Vector2 steering = v - rigidbody.velocity;
+
+            return Vector2.ClampMagnitude(steering, maxSteeringForce);
+        }
+        else return Seek(); // Ainda não encontrou o destino
+    }
+
     private void FixedUpdate()
     {
-        Vector2 steering = Seek();
+        Vector2 steering = Arrival();
         Vector2 v = rigidbody.velocity + steering;
         rigidbody.velocity = Vector2.ClampMagnitude(v, velocidadeMaxima);
 
